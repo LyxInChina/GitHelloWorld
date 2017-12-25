e155452dc309f83b3c2f369007fcd540cd075bf0
lazzyz <seainchina@163.com>
2017-12-25 17:47:00 +0800

    去掉进程注入项目中的多余项目； 添加crypto++类库引用；

--- InjectDemo/NativeDll/NativeDll.cpp
+++ InjectDemo/NativeDll/NativeDll.cpp
@@ -2,9 +2,11 @@
 
 #include <windows.h>
 #include "stdafx.h"
-#include "NativeDll.h"
-#include "MSCorEE.h"
-#include "metahost.h"
+//#include <stdio.h>
+//#include <tchar.h>
+//#include "NativeDll.h"
+//#include "MSCorEE.h"
+//#include "metahost.h"
 
 DWORD CALLBACK StartTheDotNetRuntime(LPVOID lp)
 {
@@ -37,24 +39,20 @@ DWORD CALLBACK StartTheDotNetRuntime(LPVOID lp)
 }
 
 
-BOOL WINAPI DllMain(
-  HINSTANCE hinstDLL,
-  DWORD fdwReason,
-  LPVOID lpvReserved
-)
+BOOL WINAPI DllMain(HINSTANCE hinstDLL,DWORD fdwReason,LPVOID lpvReserved)
 {
-	   switch(fdwReason)
+	switch(fdwReason)
     {
       case DLL_PROCESS_ATTACH:
 		 CreateThread(0,0,StartTheDotNetRuntime,0,0,0);
-		  //MessageBox(NULL, L"test", L"test", 0);
-		  break;
+		 //MessageBox(NULL, L"DllMain test", L"Dll Main test", 0);
+		 break;
+      case DLL_PROCESS_DETACH:
+        break;
 	  case DLL_THREAD_ATTACH:
 		 break;
 	  case DLL_THREAD_DETACH:
 		 break;
-      case DLL_PROCESS_DETACH:
-        break;
       default:
         break;
     }
