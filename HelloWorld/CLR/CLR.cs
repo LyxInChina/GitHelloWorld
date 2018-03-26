2641d32dc68a94bbb87cbc801c018a6c34b0713f
lazzyz <seainchina@163.com>
2018-03-26 16:22:42 +0800

    去掉多余的项目

--- HelloWorld/CLR/CLR.cs
+++ HelloWorld/CLR/CLR.cs
@@ -547,4 +547,68 @@ CLR头：小的信息块（托管模块特有），包含CLR版本号、标志fl
 
 
          */
+    
+    public class ConstantAndField
+    {
+        public const string CStr = "";
+        public const decimal CD = 0.22M;
+        public const ConstantAndField CCAF = null;
+        public static int sa;
+        public int ia;
+        public readonly int ra;
+        public static readonly int sra;
+
+    }
+
+    /* Chapter 7 常量和字段
+     * 
+     * 7.1  常量
+     *  常量值必须在编译时能确定，编译器将常量值保存在程序集元数据中（只能使用基元类型定义常量）
+     *  基元类型：Boolean,Char,Byte,SByte,Int16,Int32,Int64,UInt16,UInt32,UInt64,Single,Double,Decimal,String
+     *  或者非基元类型，值为null
+     *  常量被视为静态成员，定义常量将创建元数据
+     *  编译器会直接将常量值嵌入程序集的IL代码，不为常量分配内存，不能获取常量地址，不能传递引用方式传递
+     *  所以运行时从一个程序集中提取另一个程序集中的常量，应该使用readonly字段
+     *  
+     *  7.2 字段
+     *  修饰词：
+     *  static（static） Instance（默认） 
+     *  InitOnly（readonly） 只能由构造器方法中写入（可以用反射修改readonly值）
+     *  Volatile（volatile） 不执行线程不安全的优化，只能使用以下类型：引用类型，值类型基元类型，基础类型为基元值类型的枚举类型
+     *  字段何时分配内存？
+     *  字段存储在动态内存中，仅能在运行时获取
+     *  类型字段（静态字段） - 在类型对象中分配，类型对象在类型加载到第一个AppDomain时创建，
+     *      加载时间：引用了该类型的任何方法时首次进行JIT编译时；
+     *  实例字段    -   构造类型的一个实例时分配
+     *  
+     *  内联初始化：代码中直接赋值来初始化
+     *  当引用类型标记为readonly时，不可修改的是引用，并非字段引用的对象
+     
+     */
+
+    /*  Chapter 8 方法
+     * 
+     * 8.1 实例构造器和类 - 引用类型
+     * 构造器 constructor
+     * 方法定义元数据表：.ctor
+     * 创建引用类型实例过程：
+     *  1.为实例的数据字段分配内存（内存归0）；
+     *  2.初始化对象的附加字段，即类型对象指针/同步块索引；
+     *  3.调用类型的实例构造器；
+     * 
+     * 若修饰符为abstract，则可访问性为protected
+     * 若修饰符为static，编译器不会在类定义中生成默认构造器
+     * 
+     * 特殊   -   可以在不调用类型构造器时创建一个类型实例
+     * A.Object.MemberwiseClone()
+     *  1.分配内存；
+     *  2.初始化对象附加字段；
+     *  3.将源对象的字节数复制到新对象中；
+     * B.使用运行时反序列化器反序列话对象；
+     *  反序列化使用：System.Runtime.Serialization.FormatterServices的
+     *  GetUninitializedObject或者GetSafeUninitializedObject分配内存；
+     * 
+     * 
+
+     */
 }
--- HelloWorld/CLR/CSharpPerformancePromote.cs
+++ HelloWorld/CLR/CSharpPerformancePromote.cs
@@ -69,6 +69,11 @@ namespace HelloWorld.CLR
             obj.GetType();            
         }
     }
+
+    public struct MyStruct
+    {
+
+    }
 }
 
 /*
@@ -77,7 +82,14 @@ namespace HelloWorld.CLR
     函数调用：
     call与callvirt
     call：可调用静态方法（必须指定方法定义的类型），实例方法和虚方法（必须指定引用对象的变量，假定变量不为空）
-    call：可调用实例方法和虚方法（会调查发出调用的对象的类型即验证变量是否为null，然后以多态方式调用）
+    callvirt：可调用实例方法和虚方法（会调查发出调用的对象的类型即验证变量是否为null，然后以多态方式调用）
+    共同点：接收一个隐藏的this实参作为第一个参数，this实参引用要操作的对象；
 
+    调用call性能比callvirt高，call不会进行调用变量的非空判断,JIT编译器不能内嵌（inline）虚方法；
+    如何尽量让方法编译为IL后，JIT调用时，使用call而不是callvirt：
+    1.尽量使用静态类中的静态方法 - 静态方法IL代码为call；
+    2.尽量使用非虚方法，某些编译器会使用call调用非虚方法（C#编译器会使用callvirt调用所有的实例方法）；
+    3.调用值类型中的方法，一般使用call；
+    4.尽量使用sealed 密封类，JIT使用非虚方式（call）调用该类中的虚方法（C#编译器生成callvirt指令，JIT会优化这个调用）；
 
      */
